550dd4bb9c93d953740b638d3945f1dc36bd0eeb
Osama Ashaikh <hello@bayt.club>
2023-09-11 15:34:47 +0100

    Fixed workflow

--- setup.py
+++ setup.py
@@ -8,10 +8,17 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\\n" + fh.read()
 
-remote_version = subprocess.run(["git", "describe", "--tags"], stdout=subprocess.PIPE).stdout.decode('utf-8').strip()
+try:
+    remote_version = subprocess.run(
+        ["git", "describe", "--tags"],
+        stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+        check=True
+    ).stdout.decode('utf-8').strip()
+except subprocess.CalledProcessError:
+    remote_version = "0.0.0"
+
 print("remote_version: ", remote_version)
 assert "." in remote_version, "Version number is not valid"
-
 
 setup(
     name='ble_scanner',
@@ -19,7 +26,7 @@ setup(
     author="Osama Ashaikh",
     author_email="oashaikh@gmail.com",
     description="BLE Beacon scanner",
-    url = "https://github.com/oashaikh/ble-scanner",
+    url="https://github.com/oashaikh/ble-scanner",
     long_description_content_type="text/markdown",
     long_description=long_description,
     keywords=['pypi', 'BLE', 'python', 'scanner', 'beacon'],
@@ -29,4 +36,3 @@ setup(
         # other dependencies
     ],
 )
-
