617348e09b815b1e12cff982a832fb9b59a5bd4c
Osama Ashaikh <hello@bayt.club>
2023-09-11 17:18:07 +0100

    Update description

---
+++ test.py
@@ -0,0 +1,10 @@
+import asyncio
+from ble_scanner.BLEScanner import BLEScanner
+
+async def main():
+   # Create a BLEScanner instance
+    scanner = BLEScanner(target_mac="E2:15")  # Replace the MAC
+    await scanner.initialize()
+
+if __name__ == "__main__":
+    asyncio.run(main())
