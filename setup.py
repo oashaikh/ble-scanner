1ddf9995c00bb39bc7d8397d2b4708051829f5f8
Osama Ashaikh <hello@bayt.club>
2023-09-11 13:53:57 +0100

    updated setup

--- setup.py
+++ setup.py
@@ -1,11 +1,26 @@
 from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\\n" + fh.read()
 
 setup(
     name='ble_scanner',
-    version='3.2',
+    version='{{VERSION_PLACEHOLDER}}',
+    author="John Doe",
+    author_email="oashaikhe@gmail.com",
+    description="BLE Beacon scanner",
+    url = "https://github.com/oashaikh/ble-scanner",
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    keywords=['pypi', 'BLE', 'python', 'scanner', 'beacon'],
     packages=find_packages(),
     install_requires=[
         'bleak',
         # other dependencies
     ],
 )
+
